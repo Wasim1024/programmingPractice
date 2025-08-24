48805c2aa23414e9671266dce86f3deb0c81d5df
Wasim1024 <wahu4624@gmail.com>
2025-08-24 20:57:15 +0530

    Added P5 project files

---
+++ P5/data.js
@@ -0,0 +1,59 @@
+ export const playerProfile = [
+  {
+    "name": "Lionel Messi",
+    "height_m": 1.70,
+    "club": "Inter Miami CF",
+    "price_eur_millions": 180.0,
+    "id": 0,
+    "stats": {
+      "position": "Right Winger",
+      "date_of_birth": "1987-06-24"
+      
+    }
+  },
+  {
+    "name": "Neymar",
+    "height_m": 1.75,
+    "club": "Santos",
+    "price_eur_millions": 100.0,
+    "id": 1,
+    "stats": {
+      "position": "Attacking Midfield",
+      "date_of_birth": "1992-02-05"
+    }
+  },
+  {
+    "name": "Cristiano Ronaldo",
+    "id": 2,
+    "height_m": 1.88,
+    "club": "Al-Nassr FC",
+    "price_eur_millions": 12.0,
+    "stats": {
+      "position": "Centre-Forward",
+      "date_of_birth": "1985-02-05"
+    }
+  },
+  {
+    "name": "Bruno Fernandes",
+    "id": 3,
+    "height_m": 1.79,
+    "club": "Manchester United",
+    "price_eur_millions": 50.0,
+    "stats": {
+      "position": "Attacking Midfield",
+      "date_of_birth": "1994-09-08"
+    }
+  },
+  {
+    "name": "Kylian Mbappé",
+    "id": 4,
+    "height_m": 1.78,
+    "club": "Real Madrid",
+    "price_eur_millions": 100.0,
+    "stats": {
+      "position": "Centre-Forward",
+      "date_of_birth": "1998-12-20"
+    }
+  }
+]
+
