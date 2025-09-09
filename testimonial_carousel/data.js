c4dfedc78f07c3e0546bcdf3874f2059248787a3
Wasim1024 <wahu4624@gmail.com>
2025-09-09 15:54:14 +0530

    Add testimonial carousel with timer and embarrassing testimonials

---
+++ testimonial_carousel/data.js
@@ -0,0 +1,42 @@
+// Testimonial data for the carousel
+const testimonials = [
+    {
+        id: 1,
+        name: "Aarish Alam",
+        testimonial: "I once tripped over my own shoelaces during a job interview and face-planted right in front of the hiring manager. They hired me anyway, but I still cringe every time I think about it!",
+        image: "photo1.png"
+    },
+    {
+        id: 2,
+        name: "Arnab Sarma",
+        testimonial: "I showed up to a wedding in the wrong suit - it was actually my cousin's from 10 years ago. The pants were so tight I could barely sit down, and everyone kept asking if I was okay!",
+        image: "photo2.png"
+    },
+    {
+        id: 3,
+        name: "Arjunabh Das",
+        testimonial: "During a presentation, I accidentally clicked on a private folder instead of my slides. My entire screen filled with embarrassing memes I'd saved. The client laughed so hard they forgot about the actual presentation!",
+        image: "photo3.png"
+    },
+    {
+        id: 4,
+        name: "Manash Pratim Das",
+        testimonial: "I once called my boss 'Mom' during a team meeting. Everyone went silent, and I tried to play it off as a joke, but I could feel my face turning red for the next hour. Still haven't lived it down!",
+        image: "photo4.png"
+    },
+    {
+        id: 5,
+        name: "Iqbal Ahmed",
+        testimonial: "I was eating spaghetti at a fancy restaurant when a noodle flew out and hit the person at the next table. I pretended it wasn't me, but they saw the whole thing. I left a huge tip as an apology!",
+        image: "photo5.png"
+    },
+    {
+        id: 6,
+        name: "Daksh Narang",
+        testimonial: "I sent a flirty text to my boss instead of my crush. It included heart emojis and everything. I tried to delete it immediately, but she had already seen it. Now every time we meet, it's awkward!",
+        image: "photo6.png"
+    }
+];
+
+// Export the testimonials array
+export default testimonials;
